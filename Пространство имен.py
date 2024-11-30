76de5faceb48732259609ccc736112f6cadc0ab4
Casper-spirit <alaid.5@mail.ru>
2024-11-30 21:01:08 +1200

    Update Пространство имен.py

--- Пространство имен.py
+++ Пространство имен.py
@@ -1,4 +1,3 @@
-
 calls = 0
 
 def count_calls():
@@ -10,11 +9,18 @@ def string_info(string):
     return tuple_
 def is_contains(string, list_to_search):
     count_calls()
-    if string in list_to_search:
-        return True
-    else:
-        return False
+    string = string.lower()
+    for elem in list_to_search:
+        if elem.lower() == string:
+            return True
+    return False
 
-print(string_info('МозгИ'))
-print(is_contains(string = 'Взрыв мозгов', list_to_search = ['мотоцикл', 'Взрыв мозгА', 'самолет']))
+print(string_info('Capybara'))
+
+print(string_info('Armageddon'))
+
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN'])) # Urban ~ urBAN
+
+print(is_contains('cycle', ['recycling', 'cyclic'])) # No matches
+
 print(calls)
